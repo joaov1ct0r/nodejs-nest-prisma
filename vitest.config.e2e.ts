9b5621d1bfad04f39509ba29cc2435913bc4d8c9
joaov1ct0r <jov9051@outlook.com>
2024-05-26 00:00:53 -0300

    chore: test config files

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    setupFiles: ['./src/test/setup-e2e.ts'],
+    fileParallelism: false,
+    bail: 1,
+    maxConcurrency: 1,
+    retry: 1,
+  },
+  plugins: [swc.vite(), tsconfigPaths()],
+});
