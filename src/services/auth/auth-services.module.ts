c31f9653d4f903578f816fad20094f12455b24bc
joaov1ct0r <jov9051@outlook.com>
2024-05-26 05:16:55 -0300

    feat: added helpers module to auth services module

--- src/services/auth/auth-services.module.ts
+++ src/services/auth/auth-services.module.ts
@@ -5,6 +5,14 @@ import { SignInService } from '@services/auth/sign-in.service';
 import { CreateJWTServiceImp } from '@interfaces/services/auth/create-jwt-token.service';
 import { CreateJWTService } from '@services/auth/create-jwt-token.service';
 import { JwtService } from '@nestjs/jwt';
+import { CreateCookieConfigServiceImp } from '@interfaces/services/auth/create-cookie-config.service';
+import { CreateCookieConfigService } from '@services/auth/create-cookie-config.service';
+import { HelpersModule } from '@helpers/helpers.module';
+
+const createCookieConfigServiceProvider = {
+  provide: CreateCookieConfigServiceImp,
+  useClass: CreateCookieConfigService,
+};
 
 const signInServiceProvider = {
   provide: SignInServiceImp,
@@ -22,13 +30,15 @@ const jwtServiceProvider = {
 };
 
 @Module({
-  imports: [RepositoriesModule],
+  imports: [RepositoriesModule, HelpersModule],
   providers: [
+    createCookieConfigServiceProvider,
     signInServiceProvider,
     createJwtTokenServiceProvider,
     jwtServiceProvider,
   ],
   exports: [
+    createCookieConfigServiceProvider,
     signInServiceProvider,
     createJwtTokenServiceProvider,
     jwtServiceProvider,
