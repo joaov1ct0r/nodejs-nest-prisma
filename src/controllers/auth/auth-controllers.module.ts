75462582a5c51ccf8ce4d928498c8cff020023e0
joaov1ct0r <jov9051@outlook.com>
2024-05-26 05:42:54 -0300

    feat: added sign out controller to auth controllers module

--- src/controllers/auth/auth-controllers.module.ts
+++ src/controllers/auth/auth-controllers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SignInController } from '@controllers/auth/sign-in.controller';
 import { ServicesModule } from '@services/services.module';
+import { SignOutController } from '@controllers/auth/sign-out.controller';
+import { GuardsModule } from '@guards/guards.module';
 
 @Module({
-  imports: [ServicesModule],
-  controllers: [SignInController],
+  imports: [ServicesModule, GuardsModule],
+  controllers: [SignInController, SignOutController],
 })
 export class AuthControllerModule {}
