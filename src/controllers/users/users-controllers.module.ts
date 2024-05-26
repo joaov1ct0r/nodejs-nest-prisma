683e88e21f6f96990d0ffbcc07aa4f83c8d2383f
joaov1ct0r <jov9051@outlook.com>
2024-05-26 05:51:32 -0300

    feat: added get all users controller to users controller module

--- src/controllers/users/users-controllers.module.ts
+++ src/controllers/users/users-controllers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GetAllUsersController } from '@controllers/users/get-all.controller';
 import { ServicesModule } from '@services/services.module';
 import { CreateUserController } from '@controllers/users/create-user.controller';
+import { GuardsModule } from '@guards/guards.module';
 
 @Module({
-  imports: [ServicesModule],
+  imports: [ServicesModule, GuardsModule],
   controllers: [CreateUserController, GetAllUsersController],
 })
 export class UsersControllerModule {}
