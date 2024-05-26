3feb5be6a2d8656cbe10c790cc7c7ba0653807c7
joaov1ct0r <jov9051@outlook.com>
2024-05-26 02:36:38 -0300

    feat: added find user by email repository to users repositories module

--- src/repositories/users/users-repositories.module.ts
+++ src/repositories/users/users-repositories.module.ts
@@ -1,12 +1,19 @@
+import { Module } from '@nestjs/common';
 import { CountUsersRepositoryImp } from '@interfaces/repositories/users/count-users.repository';
 import { GetAllUsersRepositoryImp } from '@interfaces/repositories/users/get-all.repository';
-import { Module } from '@nestjs/common';
 import { GetAllUsersRepository } from '@repositories/users/get-all.repository';
 import { CountUsersRepository } from '@repositories/users/count-users.repository';
 import { FindUserByIdRepository } from '@repositories/users/find-user-by-id.repository';
 import { FindUserByIdRepositoryImp } from '@interfaces/repositories/users/find-user-by-id.repository';
 import { FindUserByUsernameRepository } from '@repositories/users/find-user-by-username.repository';
 import { FindUserByUsernameRepositoryImp } from '@interfaces/repositories/users/find-user-by-username.repository';
+import { FindUserByEmailRepositoryImp } from '@interfaces/repositories/users/find-user-by-email.repository';
+import { FindUserByEmailRepository } from '@repositories/users/find-user-by-email.repository';
+
+const findUserByEmailRepositoryProvider = {
+  provide: FindUserByEmailRepositoryImp,
+  useClass: FindUserByEmailRepository,
+};
 
 const findUserByUsernameRepositoryProvider = {
   provide: FindUserByUsernameRepositoryImp,
@@ -30,12 +37,14 @@ const countUsersRepositoryProvider = {
 
 @Module({
   exports: [
+    findUserByEmailRepositoryProvider,
     findUserByUsernameRepositoryProvider,
     findUserByIdRepositoryProvider,
     countUsersRepositoryProvider,
     getAllUsersRepositoryProvider,
   ],
   providers: [
+    findUserByEmailRepositoryProvider,
     findUserByUsernameRepositoryProvider,
     findUserByIdRepositoryProvider,
     countUsersRepositoryProvider,
