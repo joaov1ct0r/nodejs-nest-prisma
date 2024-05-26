48081a368cf49c3eaa121d1f4f33cffe356420b5
joaov1ct0r <jov9051@outlook.com>
2024-05-26 00:24:26 -0300

    feat: base controller

---
+++ src/controllers/base.controller.ts
@@ -0,0 +1,14 @@
+import { BadRequestException } from '@nestjs/common';
+import { CreateEventLogServiceImp } from '@services/logs/create-event-log.service';
+
+export abstract class BaseController {
+  protected readonly _createEventLogService: CreateEventLogServiceImp;
+
+  constructor(createEventLogService: CreateEventLogServiceImp) {
+    this._createEventLogService = createEventLogService;
+  }
+
+  protected badRequest(message: string): BadRequestException {
+    return new BadRequestException(message);
+  }
+}
