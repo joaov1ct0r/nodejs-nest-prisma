fced2eb65882ebef687b4699640c0662fddf4737
joaov1ct0r <jov9051@outlook.com>
2024-05-26 05:19:58 -0300

    feat: added auth controllers to auth module

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersControllerModule } from '@controllers/users/users-controllers.module';
+import { AuthControllerModule } from '@controllers/auth/auth-controllers.module';
 
 @Module({
-  imports: [UsersControllerModule],
+  imports: [UsersControllerModule, AuthControllerModule],
 })
 export class ControllersModule {}
