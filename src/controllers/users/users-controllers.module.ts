0d12176d2e143f42ece8e4cae4dbfbe6ff9f88dd
joaov1ct0r <jov9051@outlook.com>
2024-05-26 18:11:46 -0300

    feat: added delete user controller to users controllers module

--- src/controllers/users/users-controllers.module.ts
+++ src/controllers/users/users-controllers.module.ts
@@ -3,9 +3,14 @@ import { GetAllUsersController } from '@controllers/users/get-all.controller';
 import { ServicesModule } from '@services/services.module';
 import { CreateUserController } from '@controllers/users/create-user.controller';
 import { GuardsModule } from '@guards/guards.module';
+import { DeleteUserController } from '@controllers/users/delete-user.controller';
 
 @Module({
   imports: [ServicesModule, GuardsModule],
-  controllers: [CreateUserController, GetAllUsersController],
+  controllers: [
+    CreateUserController,
+    GetAllUsersController,
+    DeleteUserController,
+  ],
 })
 export class UsersControllerModule {}
