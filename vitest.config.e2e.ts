be3a70a90818c63c2b007d5bf93218d20b26c139
joaov1ct0r <jov9051@outlook.com>
2024-05-26 00:40:08 -0300

    chore: added /data folder to gitignore

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
-    include: ['**/*.e2e-spec.ts'],
+    include: ['./src/**/*.e2e-spec.ts'],
     globals: true,
     root: './',
     setupFiles: ['./src/test/setup-e2e.ts'],
