ccac35d27096f66c83eed72e1f30e01eeef59f54
joaov1ct0r <jov9051@outlook.com>
2024-05-26 04:13:21 -0300

    refactor: services module

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersServicesModule } from '@services/users/users-services.module';
 import { AuthorizationServicesModule } from '@services/auth/auth-services.module';
-import { EventLogServicesModule } from './logs/event-log-services.module';
+import { EventLogServicesModule } from '@services/logs/event-log-services.module';
 
 @Module({
   exports: [
