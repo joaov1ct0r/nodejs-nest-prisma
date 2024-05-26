8543718e9411c778bc3a6c552def6af8603445f2
joaov1ct0r <jov9051@outlook.com>
2024-05-26 00:24:10 -0300

    feat: added logs, users and auth services to services module

---
+++ src/services/services.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { UsersServicesModule } from '@services/users/users-services.module';
+import { AuthorizationServicesModule } from '@services/auth/auth-services.module';
+import { EventLogServicesModule } from './logs/event-log-services.module';
+
+@Module({
+  exports: [
+    UsersServicesModule,
+    AuthorizationServicesModule,
+    EventLogServicesModule,
+  ],
+  imports: [
+    UsersServicesModule,
+    AuthorizationServicesModule,
+    EventLogServicesModule,
+  ],
+})
+export class ServicesModule {}
